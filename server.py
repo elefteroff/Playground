54deb7c2ed01cb7c8003796778c7fb0de35c924a
elefteroff <elefteroff@mac.com>
2021-09-04 11:36:13 -0700

    Initial Commit

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template 
+app = Flask(__name__)    
+
+@app.route('/play')
+def play():
+    return render_template('index.html', num = 3)
+
+@app.route('/play/<int:x>')
+def playx(x):
+    return render_template('index.html', num = x)
+
+@app.route('/play/<int:x>/<color>')
+def play_color(x, color):
+    return render_template('index.html',num = x, color = color)
+
+if __name__=="__main__":     
+    app.run(debug=True)    
+
